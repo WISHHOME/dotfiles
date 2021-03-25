888068896c17518b5d054497e649d9296cf80b2f
hyungseok <hs.park@wishhome.co.kr>
2021-03-25 15:26:37 +0900

    feat: add installing vscode extensions script

---
+++ install-vscode-extensions.zsh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+code-insiders --install-extension visualstudioexptteam.vscodeintellicode
+code-insiders --install-extension ms-vsliveshare.vsliveshare
+code-insiders --install-extension msjsdiag.debugger-for-chrome
+code-insiders --install-extension ms-vscode-remote.remote-containers
+code-insiders --install-extension ms-vscode.vscode-typescript-tslint-plugin
+code-insiders --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
+code-insiders --install-extension visualstudioonlineapplicationinsights.application-insights
+code-insiders --install-extension ms-vscode-deploy-azure.azure-deploy
+code-insiders --install-extension azuredevspaces.azds
+code-insiders --install-extension mindaro.mindaro
+code-insiders --install-extension ms-vscode.vscode-node-azure-pack
+code-insiders --install-extension ms-kubernetes-tools.vscode-aks-tools
+code-insiders --install-extension ms-edgedevtools.vscode-edge-devtools
+code-insiders --install-extension docsmsft.docs-authoring-pack
