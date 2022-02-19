d7755d4e5d09b219dcad9a05c8fe70f4fefa1c04
Jason Park <binarydiver@outlook.com>
2022-02-19 22:24:17 +0900

    refactor: update vscode extension

--- install-vscode-extensions.zsh
+++ install-vscode-extensions.zsh
@@ -1,23 +1,36 @@
 #!/bin/zsh
 
-code --install-extension visualstudioexptteam.vscodeintellicode
-code --install-extension ms-vsliveshare.vsliveshare
-code --install-extension msjsdiag.debugger-for-chrome
-code --install-extension ms-vscode-remote.remote-containers
-code --install-extension ms-vscode.vscode-typescript-tslint-plugin
-code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
-code --install-extension visualstudioonlineapplicationinsights.application-insights
-code --install-extension ms-vscode-deploy-azure.azure-deploy
-code --install-extension azuredevspaces.azds
-code --install-extension mindaro.mindaro
-code --install-extension ms-vscode.vscode-node-azure-pack
-code --install-extension ms-kubernetes-tools.vscode-aks-tools
-code --install-extension ms-edgedevtools.vscode-edge-devtools
-code --install-extension docsmsft.docs-authoring-pack
-
+# angular
 code --install-extension angular.ng-template
 code --install-extension infinity1207.angular2-switcher
-code --install-extension esbenp.prettier-vscode
-code --install-extension dbaeumer.vscode-eslint
 
+# devops
+code --install-extension azuredevspaces.azds
+code --install-extension mindaro.mindaro
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-kubernetes-tools.vscode-aks-tools
+code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
+code --install-extension ms-vscode-deploy-azure.azure-deploy
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vscode.vscode-node-azure-pack
+code --install-extension visualstudioonlineapplicationinsights.application-insights
+
+# fun
+code --install-extension vscode-icons-team.vscode-icons
+
+# git
+code --install-extension eamodio.gitlens
+
+# lint
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension docsmsft.docs-authoring-pack
+code --install-extension esbenp.prettier-vscode
+code --install-extension ms-vscode.vscode-typescript-tslint-plugin
+
+# utilities
 code --install-extension gruntfuggly.todo-tree
+code --install-extension MS-vsliveshare.vsliveshare
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+
+# web
+code --install-extension ms-edgedevtools.vscode-edge-devtools
