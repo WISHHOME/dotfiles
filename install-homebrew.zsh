062328d861fa409e2390b3239e8556fa9c6990c7
Hyungseok <hs.park@wishhome.co.kr>
2022-02-21 11:11:04 +0900

    refactor: add some script to run brew

--- install-homebrew.zsh
+++ install-homebrew.zsh
@@ -2,4 +2,7 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$(id -un)/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 brew tap Homebrew/bundle
